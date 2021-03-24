46ac125216cb0aa4fde4be35c966290dbbf9c881
TLJParadox <xxrpp@qq.com>
2021-03-24 20:27:24 +0800

    统一属性VMtype

--- VirtualMachine.py
+++ VirtualMachine.py
@@ -1,6 +1,6 @@
 class VirtualMachine:
-    def __init__(self, vtype="", cpuCore = 0, ram = 0, deploy = 0, vmId = 0):
-        self.vtype = vtype
+    def __init__(self, vmType="", cpuCore = 0, ram = 0, deploy = 0, vmId = 0):
+        self.vmType = vmType
         self.cpuCore = cpuCore
         self.ram = ram
         self.deploy = deploy
