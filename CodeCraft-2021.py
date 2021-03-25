25c493eff12598d3d03074876a0cad51dd2b335c
TLJParadox <xxrpp@qq.com>
2021-03-25 16:04:44 +0800

    简单的添加删除代码

---
+++ CodeCraft-2021.py
@@ -0,0 +1,207 @@
+from readfile import readFile, input2format , input2formatstd
+import copy
+import sys
+
+class Optimization:
+    def __init__(self,ServerList, VMList, CommandList):
+        self.ServerList = ServerList
+        self.VMList = VMList
+        self.CommandList = CommandList
+        self.totalResources = {}  #当天的总资源
+        self.alreadyBuyServerInfo = {}  #已经买了的服务器 <id,server>
+        self.vmID2vmType = {}  # 虚拟机ID对虚拟机型号 <vmid, vmtype>
+        self.vmID2sid = {}  #虚拟机ID与服务器ID <vmid, serverid>
+        self.vmID2End = {}  #虚拟机部署的对应表  <vmid, 1/2>
+    
+    def optimization(self):
+        cnt = 0
+        res = []
+        days = len(CommandList)
+        for i in range(days):
+            numsBuyToday = {}  #每天购买 <stype, nums>
+            buyHashToday = {}  #申请编号和当天服务器编号 <id, sid>
+            buyOrderToday = []  #当天买的服务器 在ServerList中的第几个型号
+            tempRes = []
+            buyCntToday = 0
+            # 扫描第i天的所有请求
+            for j in range(len(CommandList[i])):
+                tempCommand = CommandList[i][j]
+                
+                # add
+                if tempCommand.commandType == 'add':
+                    self.vmID2vmType[tempCommand.vmId] = tempCommand.vmType #建立 虚拟机节点和型号对应 <vmid, vmType>
+                    if tempCommand.vmId == 307089369:
+                        print("haoye")
+                    bothFlag = VMList[tempCommand.vmType].deploy #按该虚拟机节点部署
+                    cpuCore = VMList[tempCommand.vmType].cpuCore
+                    ram = VMList[tempCommand.vmType].ram
+
+                    availableFlag = False  #判断是否在已有服务器能部署的下
+                    if bothFlag:  #A,B双节点部署
+                        for key,value in self.alreadyBuyServerInfo.items(): #获取服务器ID,服务器参数
+                            # 如果空间足够，能够部署
+                            if value.availableCpuCoreA >= cpuCore / 2 and value.availableRamA >= ram / 2 \
+                                and value.availableCpuCoreB >= cpuCore / 2 and value.availableRamB >= ram / 2:
+                                tempStr = '(' + str(key) + ')'
+                                tempRes.append(tempStr)
+                                value.availableCpuCoreA -= cpuCore / 2
+                                value.availableRamA -= ram / 2
+                                value.availableCpuCoreB -= cpuCore / 2
+                                value.availableRamB -= ram / 2
+                                availableFlag = True
+                                self.vmID2sid[tempCommand.vmId] = key
+                                break
+                        #不要购买，直接申请
+                        if availableFlag:
+                            continue
+                        #服务器空间不足，需要购买
+                        #查询服务器产品列表
+                        for k in range(len(ServerList)):
+                            server = copy.deepcopy(ServerList[k])
+                            #如果当前服务器满足
+                            if server.availableCpuCoreA >= cpuCore / 2 and server.availableRamA >= ram / 2 \
+                                and server.availableCpuCoreB >= cpuCore / 2 and server.availableRamB >= ram / 2:
+                                buyCntToday += 1 #当天购买数量加1
+                                # 当前cnt为空余id，给买的server
+                                self.alreadyBuyServerInfo[cnt] = server    # <id, server>
+                                self.alreadyBuyServerInfo[cnt].availableCpuCoreA -= cpuCore / 2
+                                self.alreadyBuyServerInfo[cnt].availableRamA -= ram / 2
+                                self.alreadyBuyServerInfo[cnt].availableCpuCoreA -= cpuCore / 2
+                                self.alreadyBuyServerInfo[cnt].availableRamB -= ram / 2
+                                buyHashToday[cnt] = k
+                                if k not in buyOrderToday:
+                                    buyOrderToday.append(k)
+                                if not numsBuyToday.get(k): 
+                                    numsBuyToday[k] = 0
+                                numsBuyToday[k] += 1
+                                tempStr = '(' + str(cnt) + ')'
+                                tempRes.append(tempStr)
+                                self.vmID2sid[tempCommand.vmId] = cnt 
+                                cnt += 1 #当前服务器ID增加1，给之后新的服务器 
+                                break
+                    #单节点部署
+                    else:
+                        for key,value in self.alreadyBuyServerInfo.items():
+                            # 如果A节点有空间
+                            if value.availableCpuCoreA >= cpuCore and value.availableRamA >= ram :
+                                tempStr = '(' + str(key) + ", A)"
+                                tempRes.append(tempStr)
+                                value.availableCpuCoreA -= cpuCore
+                                value.availableRamA -= ram
+                                availableFlag = True # 能够部署，不需要购买
+                                self.vmID2End[tempCommand.vmId] = 1  #虚拟机部署在A节点  <vmID, 1>
+                                self.vmID2sid[tempCommand.vmId] = key
+                                break
+                            elif value.availableCpuCoreB >= cpuCore and value.availableRamB >= ram :
+                                tempStr = '(' + str(key) + ", B)"
+                                tempRes.append(tempStr)
+                                value.availableCpuCoreB -= cpuCore
+                                value.availableRamB -= ram
+                                availableFlag = True # 能够部署，不需要购买
+                                self.vmID2End[tempCommand.vmId] = 2  #虚拟机部署在B节点  <vmID, 2>
+                                self.vmID2sid[tempCommand.vmId] = key
+                                break
+                        if availableFlag:
+                            continue #填充进A节点 不进行后续操作
+                        for k in range(len(ServerList)):
+                            #买一个服务器，默认放在A节点
+                            if ServerList[k].availableCpuCoreA >= cpuCore and ServerList[k].availableRamA >= ram:
+                                buyCntToday += 1 #当天购买数量+1
+                                self.alreadyBuyServerInfo[cnt] = copy.deepcopy(ServerList[k])
+                                self.alreadyBuyServerInfo[cnt].availableCpuCoreA -= cpuCore
+                                self.alreadyBuyServerInfo[cnt].availableRamA -= ram
+                                buyHashToday[cnt] = k #购买的服务器id 对应 第k个服务型号
+                                if k not in buyOrderToday:
+                                    buyOrderToday.append(k)
+                                if not numsBuyToday.get(k): #第k个型号的服务器没有买
+                                    numsBuyToday[k] = 0 #初始化
+                                numsBuyToday[k] += 1
+                                tempStr = '(' + str(cnt) + ", A)"
+                                tempRes.append(tempStr)
+                                self.vmID2End[tempCommand.vmId] = 1
+                                self.vmID2sid[tempCommand.vmId] = cnt
+                                cnt += 1
+                                break
+                # 'del'操作
+                else:     
+                    delVmId = tempCommand.vmId
+                    if tempCommand.vmId == 307089369:
+                        print("haoye")
+                    serverId = self.vmID2sid[delVmId]
+                    # 对应服务器ID资源恢复
+                    tempVmInfo = self.VMList[self.vmID2vmType[delVmId]]
+                    # 双节点删除操作
+                    if tempVmInfo.deploy:
+                        self.alreadyBuyServerInfo[serverId].availableCpuCoreA += tempVmInfo.cpuCore / 2
+                        self.alreadyBuyServerInfo[serverId].availableRamA += tempVmInfo.ram / 2
+                        self.alreadyBuyServerInfo[serverId].availableCpuCoreA += tempVmInfo.cpuCore / 2
+                        self.alreadyBuyServerInfo[serverId].availableRamB += tempVmInfo.ram / 2
+                        self.vmID2vmType.pop(delVmId)
+                    else:
+                        # 部署在节点A
+                        if self.vmID2End[delVmId] == 1:
+                            self.alreadyBuyServerInfo[serverId].availableCpuCoreA += tempVmInfo.cpuCore / 2
+                            self.alreadyBuyServerInfo[serverId].availableRamA += tempVmInfo.ram / 2
+                        else:
+                            self.alreadyBuyServerInfo[serverId].availableCpuCoreB += tempVmInfo.cpuCore / 2
+                            self.alreadyBuyServerInfo[serverId].availableRamB += tempVmInfo.ram / 2
+                        self.vmID2vmType.pop(delVmId)
+                        self.vmID2End.pop(delVmId)
+            tCnt = sIndex = len(self.alreadyBuyServerInfo) - buyCntToday;
+            tempServerInfos = {}
+            Nmap = {}
+            for k1 in range(len(buyOrderToday)): #遍历所有买的服务器型号
+                for k2 in range(len(buyHashToday)):
+                    tempServerNumber = buyHashToday[k2 + sIndex]
+                    if tempServerNumber == buyOrderToday[k1]:
+                        Nmap[k2 + sIndex] = tCnt
+                        tempServerInfos[tCnt] = self.alreadyBuyServerInfo[k2 + sIndex]
+                        tCnt += 1
+            
+            for k in range(len(buyHashToday)):
+                self.alreadyBuyServerInfo[k + sIndex] = tempServerInfos[ k + sIndex]
+            
+            for key,value in self.vmID2sid.items(): #虚拟机对应的服务器ID需要改变
+                if value < sIndex:
+                    continue
+                value = Nmap[value]
+            
+            resForRequest = []
+            for tres in tempRes:
+                tNum = 0
+                for t in range(1,len(tres)):
+                    if tres[t] >= '0' and tres[t] <= '9':
+                        tNum = tNum * 10 + int(tres[t]) 
+                    else:
+                        break
+                if tNum < sIndex:
+                    resForRequest.append(tres)
+                    continue
+                tempStr = '(' + str(Nmap[tNum]) + tres[t:]
+                resForRequest.append(tempStr)
+            # purchase输出信息
+            res.append("(purchase, " + str(len(buyOrderToday)) + ")")
+            for j in range(0, len(buyOrderToday)):
+                tempStr = "(" + self.ServerList[buyOrderToday[j]].stype + ", " + str(numsBuyToday[buyOrderToday[j]]) + ")"
+                res.append(tempStr)
+            
+            # migration输出信息
+            res.append("(migration, 0)")
+            for tres in resForRequest:
+                res.append(tres)
+            for r in res:
+                print(r)
+                sys.stdout.flush()
+
+        
+if __name__ == "__main__":
+    filepath = 'training-1.txt'
+    inputList = readFile(filepath)
+    ServerList, VMList, CommandList = input2format(inputList)
+    #ServerList, VMList, CommandList = input2formatstd()
+    op = Optimization(ServerList, VMList, CommandList)
+    op.optimization()
+    
+
+    
+    
--- readfile.py
+++ readfile.py
@@ -21,13 +21,13 @@ def input2format(inputList):
 
     M = int(inputList[N+1])
     #virtualMachine = inputList[N+2:M+N+2]
-    VMList = [] #存储虚拟机信息
+    VMList = {} #存储虚拟机信息
     for i in range(N+2,M+N+2):
         snew = inputList[i].replace(" ","")
         snew = snew[1:-1]
         s = snew.split(',')
         vm = VirtualMachine(s[0],int(s[1]),int(s[2]),int(s[3]))
-        VMList.append(vm)
+        VMList[vm.vmType] = vm
 
     T = int(inputList[M+N+2])
     request = []
@@ -38,6 +38,7 @@ def input2format(inputList):
     while index < len(inputList):
         R = int(inputList[index])
         #requestDay = inputList[index+1:index+R+1]
+        CommandDayList = []
         for i in range(index+1, index+R+1):
             snew = inputList[i].replace(" ","")
             snew = snew[1:-1]
@@ -52,10 +53,57 @@ def input2format(inputList):
         index = index + R + 1
     return ServerList, VMList, CommandList # 返回服务器、虚拟机、命令信息
 
+def input2formatstd():
+    N = int(input())
+    #server = inputList[1:N+1]
+    ServerList = [] #存储服务器信息
+    for i in range(0,N):
+        snew = input()
+        snew = snew.replace(" ","")
+        snew = snew[1:-1]
+        s = snew.split(',')
+        serv = Server(s[0],int(s[1]),int(s[2]),int(s[3]),int(s[4]))
+        ServerList.append(serv)
+
+    M = int(input())
+    #virtualMachine = inputList[N+2:M+N+2]
+    VMList = {} #存储虚拟机信息
+    for i in range(N+2,M+N+2):
+        snew = input()
+        snew = snew.replace(" ","")
+        snew = snew[1:-1]
+        s = snew.split(',')
+        vm = VirtualMachine(s[0],int(s[1]),int(s[2]),int(s[3]))
+        VMList[vm.vmType] = vm
+
+    T = int(input())
+    request = []
+    day = 0
+    CommandList = []  #存储T天的命令信息 
+    CommandDayList = []
+    while day < T:
+        R = int(input())
+        #requestDay = inputList[index+1:index+R+1]
+        CommandDayList = []
+        for i in range(0, R):
+            snew = input()
+            snew = snew.replace(" ","")
+            snew = snew[1:-1]
+            s = snew.split(',')
+            if len(s) == 2:
+                command = Command(s[0],"",int(s[1]))
+            else:
+                command = Command(s[0],s[1],int(s[2]))
+            CommandDayList.append(command)
+        CommandList.append(CommandDayList)
+        #request.append([R,requestDay])
+        day += 1
+    return ServerList, VMList, CommandList # 返回服务器、虚拟机、命令信息
+    
 if __name__ == '__main__':
-    filepath = 'training-1.txt'
-    inputList = readFile(filepath)
-    ServerList, VMList, CommandList = input2format(inputList)
+    #filepath = 'training-1.txt'
+    #inputList = readFile(filepath)
+    ServerList, VMList, CommandList = input2formatstd()
     print(ServerList[0].stype)
     
     
